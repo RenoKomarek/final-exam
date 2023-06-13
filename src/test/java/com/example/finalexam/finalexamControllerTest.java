dbcf8ab4fa6554c7d7bb232aeaa6223b13f106f6
Dideldidu <ewfccfcvw@ievmi.com>
2023-06-13 10:38:00 +0200

    This commit will add a counter in the code of the digitsum function and the usage function and a unit test

--- src/test/java/com/example/finalexam/finalexamControllerTest.java
+++ src/test/java/com/example/finalexam/finalexamControllerTest.java
@@ -14,4 +14,18 @@ class finalexamControllerTest {
         Integer expected = 10;
         assertEquals(expected, finalexamController.digitsum(given));
     }
+
+    @Test
+    void usage() {
+        Integer test1 = 3456;
+        Integer test2 = 5463;
+        Integer test3 = 6439;
+        Integer expected2 = 3;
+
+        finalexamController.digitsum(test1);
+        finalexamController.digitsum(test2);
+        finalexamController.digitsum(test3);
+
+        assertEquals(expected2, finalexamController.usage());
+    }
 }
